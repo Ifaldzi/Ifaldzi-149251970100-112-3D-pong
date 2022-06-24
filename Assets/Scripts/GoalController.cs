5d38c2013daf2fc8d9d131d11368008be4afb541
Ifaldzi <ifaldzialwihudhori@gmail.com>
2022-06-24 11:15:42 +0700

    Score mechanic
    Add end condition when player reach max score

--- Assets/Scripts/GoalController.cs
+++ Assets/Scripts/GoalController.cs
@@ -4,14 +4,28 @@ using UnityEngine;
 
 public class GoalController : MonoBehaviour
 {
-    public ScoreManager.PlayerNumber player;
+    public ScoreManager.PlayerNumber playerNumber;
+    public GameObject wall;
+    public GameObject playerPaddle;
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Ball"))
         {
-            ScoreManager.Instance.AddPlayerScore((int) player, 1);
+            ScoreManager.Instance.AddPlayerScore((int) playerNumber, 1);
             BallManager.Instance.IncreaseBallCount(-1);
             other.gameObject.SetActive(false);
+
+            if (ScoreManager.Instance.IsPlayerScoreMax(playerNumber))
+            {
+                GameOver();
+            }
         }
+    }
+
+    private void GameOver()
+    {
+        gameObject.SetActive(false);
+        wall.SetActive(true);
+        playerPaddle.SetActive(false);
     }
 }
--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -12,6 +12,8 @@ public class ScoreManager : MonoBehaviour
         PLAYER_FOUR = 3 
     }
 
+    public int maxScore;
+
     public static ScoreManager Instance;
 
     public int[] playerScores = { 0, 0, 0, 0 };
@@ -24,10 +26,52 @@ public class ScoreManager : MonoBehaviour
     public void AddPlayerScore(int playerNumber, int increment)
     {
         playerScores[playerNumber] += increment;
+
+        if (IsGameOver())
+        {
+            GameOver();
+        }
     }
 
     public int GetPlayerScore(PlayerNumber playerNumber)
     {
         return playerScores[(int)playerNumber];
+    }
+
+    public bool IsPlayerScoreMax(PlayerNumber playerNumber)
+    {
+        return GetPlayerScore(playerNumber) >= maxScore;
+    }
+
+    private bool IsGameOver()
+    {
+        int totalActivePlayer = playerScores.Length;
+        foreach (int score in playerScores)
+        {
+            if (score >= maxScore)
+                totalActivePlayer--;
+        }
+
+        if (totalActivePlayer > 1)
+            return false;
+
+        return true;
+    }
+
+    private void GameOver()
+    {
+        int winner = FindWinningPlayer();
+        Debug.Log("Game Over, Win player " + winner);
+    }
+
+    private int FindWinningPlayer()
+    {
+        for(int i = 0; i < playerScores.Length; i++)
+        {
+            if (playerScores[i] < maxScore)
+                return i + 1;
+        }
+
+        return 0;
     }
 }
